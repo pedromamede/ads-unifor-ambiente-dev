ea97dc29797fd4064dbd21266514a2605fa0c6ec
Pedro Mamede <pedro.mamede@gmail.com>
2022-11-24 12:45:08 -0300

    implementacao de duas classes usando funcoes da classe Calculadora

---
+++ src/CalculandoPiso.java
@@ -0,0 +1,8 @@
+public class CalculandoPiso{
+  public static void main(String args []){
+    Calculadora calculadora = new Calculadora();
+    double resultadoPiso = calculadora.piso(4.6);
+    System.out.println("Mostrando resultado do piso de 4.6:");
+    System.out.println(resultadoPiso);
+  }
+}
---
+++ src/CalculandoRaizQuadrada.java
@@ -0,0 +1,8 @@
+public class CalculandoRaizQuadrada{
+  public static void main(String args []){
+    Calculadora calculadora = new Calculadora();
+    double resultadoRaizQuadrada = calculadora.raizQuadrada(9);
+    System.out.println("Mostrando resultado da raiz quadrada de 9:");
+    System.out.println(resultadoRaizQuadrada);
+  }
+}
