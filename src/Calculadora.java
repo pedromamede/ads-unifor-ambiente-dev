6c4b59b811c9cb43d7b583464ca6a200fe665c8e
samuelVRcoder <118441330+samuelVRcoder@users.noreply.github.com>
2022-11-24 15:10:44 -0300

    funcoes-multiplicacao-e-divisao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -8,11 +8,11 @@ public class Calculadora {
   }
 
   public double divisao(double numero1, double numero2){
-    return 0;
+    return numero1 / numero2;
   }
 
   public double multiplicacao(double numero1, double numero2){
-    return 0;
+    return numero1 * numero2;
   }
 
   public double exponencial(double numero, double potencia) {
