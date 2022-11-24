e909f3fd89b8e33bf1e073e1f1a31b77bd6198cc
Pedro Mamede <pedro.mamede@gmail.com>
2022-11-24 12:20:23 -0300

    colocando retorno generico nas funcoes da calculadora

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         Calculadora calculadora = new Calculadora();
         double resultado = calculadora.soma(2,3);
         System.out.println(resultado);
--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,33 +1,33 @@
 public class Calculadora {
   public double soma(double numero1, double numero2){
-    
+    return 0;
   }
 
   public double subtracao(double numero1, double numero2){
-
+    return 0;
   }
 
   public double divisao(double numero1, double numero2){
-
+    return 0;
   }
 
   public double multiplicacao(double numero1, double numero2){
-
+    return 0;
   }
 
   public double exponencial(double numero, double potencia) {
-	
+    return 0;
   }
 
   public double raizQuadrada(double numero){
-
+    return 0;
   }
 
   public int piso(double numero){
-
+    return 0;
   }
 
   public int teto(double numero){
-
+    return 0;
   }
 }
